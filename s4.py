4464d308c4ff5f67b17b6de12c7841db6665a501
siddhant jain <70528434+Sid123-jain@users.noreply.github.com>
2022-12-01 11:26:47 +0530

    Add files via upload

---
+++ s4.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+get_ipython().system('pip install streamlit')
+
+
+# In[4]:
+
+
+import pickle
+import streamlit as st
+
+model=pickle.load(open('sentiment_analysis_model.p','rb'))
+
+st.title(' Sentiment Analysis Model ')
+
+st.write('Enter text for sentiment analysis:')
+message = st.text_area("","Type Here ...")
+if st.button('PREDICT'):
+    disp=" "
+    a = model.predict([message])[0]
+    if(a == 'pos'):
+        disp = "Positive Sentiment!"
+    else:
+        disp = "Negative Sentiment!"
+    st.write('The sentiment of given text is:', disp)
+    # st.header(f"**{a}**")
+    # q = model.predict_proba([message])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
